c1a52cd15cc31f58c677b00a7911ae8989496e04
Alexandr Alexandrow <limasowalexndr@gmail.com>
2018-05-15 19:48:41 +0300

    accordeon 2 section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+/*Полноэкранное меню*/
+
 const hamburger__menu = document.querySelector('.hamburger__menu');
 const menu__mob = document.querySelector('.menu__mob');
 const mob__close = document.querySelector('.mob__close');
@@ -15,3 +17,57 @@ mob__close.addEventListener('click', function(event) {
   hamburger__menu.style.display = '';
   document.body.style.overflow = '';
 })
+
+/*Секция "Меню" аккордеон (доработать функционал)*/
+
+const acco = document.querySelector('.menu');
+const accoItem = document.querySelectorAll('.menu__accordion-item');
+
+acco.addEventListener('click', function(e) {
+  for (let i = 0; i < accoItem.length; i++) {
+    accoItem[i].classList.remove('menu__accordion-item--active');
+  }
+})
+
+for (let i = 0; i < accoItem.length; i++) {
+  accoItem[i].addEventListener('click', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (accoItem[i].classList.contains('menu__accordion-item--active')) {
+      accoItem[i].classList.remove('menu__accordion-item--active');
+    } 
+    else {      
+    for (let i = 0; i < accoItem.length; i++) {
+      if (accoItem[i].classList.contains('menu__accordion-item--active')) {
+      accoItem[i].classList.remove('menu__accordion-item--active');
+      }  
+    }  
+    accoItem[i].classList.add('menu__accordion-item--active');
+    }
+  })
+}
+
+/*Секция "Команда" аккордеон*/
+
+const team = document.querySelector('.team__block');
+const teamItem = document.querySelectorAll('.accordion__item');
+
+for (let i = 0; i < teamItem.length; i++) {
+  teamItem[i].addEventListener('click', function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (teamItem[i].classList.contains('accordion__item--active')) {
+      teamItem[i].classList.remove('accordion__item--active');
+    } 
+    else {      
+    for (let i = 0; i < teamItem.length; i++) {
+      
+        teamItem[i].classList.remove('accordion__item--active');
+      }  
+      
+    teamItem[i].classList.add('accordion__item--active');
+    }
+  })
+}
